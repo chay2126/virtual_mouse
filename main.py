a0592478ca944bee9771335f72111b1887f4f4fe
chaitanya pentakota <chaitupentakota1999@gmail.com>
2026-03-08 13:36:36 +0530

    now by adding this feature i can move my mouse using index finger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import pyautogui
 from hand_tracking.hand_tracker import HandTracker
 
 
@@ -7,6 +8,8 @@ def main():
     cap = cv2.VideoCapture(0)
 
     tracker = HandTracker()
+
+    screen_w, screen_h = pyautogui.size()
 
     while True:
 
@@ -19,6 +22,18 @@ def main():
         if landmarks:
 
             fingers = tracker.get_fingers_up(landmarks)
+
+            # index finger tip
+            x = landmarks[8][1]
+            y = landmarks[8][2]
+
+            frame_h, frame_w, _ = frame.shape
+
+            screen_x = int(x * screen_w / frame_w)
+            screen_y = int(y * screen_h / frame_h)
+
+            if fingers[1] == 1 and fingers[2] == 0:
+                pyautogui.moveTo(screen_x, screen_y)
 
             print(fingers)
 
